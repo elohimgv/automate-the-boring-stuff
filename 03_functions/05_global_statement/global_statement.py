dcd9454f7bbc1e07344834939cf5ce8fc2e00947
elohimgv <elohim.guevara@gmail.com>
2020-01-11 11:05:03 -0600

    add 01_hello_func/hello_func.py add 02_hello_func2/hello_func2.py add 03_magic_8_ball/magic_8_ball.py add 04_local_and_global_vars/local_and_global_vars.py add 05_global_statement/global_statement.py add 06_global_statement2/global_statement2.py add 07_local_var/local_var.py add 08_zero_divide/zero_divide.py add 09_guess_the_number/guess_the_number.py add 10_collatz_sequence/collatz_sequence.py
    belongs to: 03_functions

---
+++ 03_functions/05_global_statement/global_statement.py
@@ -0,0 +1,13 @@
+# Program name: global_statement.py
+
+# When you have a line such as global eggs at the
+# top of a function, it tells the Python, 'in this
+# function, eggs refers to the global variable,
+# so don't create a local variable with this name.'
+def spam():
+    global eggs
+    eggs = 'spam'
+
+eggs = 'global'
+spam()
+print(eggs)
---
+++ 03_functions/07_local_var/local_var.py
@@ -0,0 +1,10 @@
+# Program name: local_var.py
+
+# This cause an error because we need to declare eggs
+# variable before use in print function.
+def spam():
+    print(eggs) # ERROR!
+    eggs = 'spam local'
+
+eggs = 'global'
+spam()
