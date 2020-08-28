df9bfe4d0acd4db730ee5b7e92d8699a2ab68dbb
elohimgv <elohim.guevara@gmail.com>
2020-08-28 13:21:38 -0500

    08_optional_matching/optional_matching.py
    belongs to: 07_pattern_matching_with_regular_expressions

---
+++ 07_pattern_matching_with_regular_expressions/08_optional_matching/optional_matching.py
@@ -0,0 +1,25 @@
+# Program name: optional_matching.py
+#Import module re
+import re
+
+string = "Remember we are working with REGEX"
+print(string.upper())
+
+# Creating a regex object
+plenetRegex = re.compile(r'Satur(na)+no') # Matches 0 or more repetitions of the preceding RE
+
+while True:
+    txt = input("What is the sixth planet on the solar sistem? ")
+    # Call the search() method to find the pattern
+    mo = plenetRegex.search(txt)
+    # Call the match object group
+    if txt == 'Saturno': # Cause an error...
+        print("%r is the correct answer! XD" % mo.group())
+    else:
+        print("Although %r is no the correct answer is acceptable for 'Regex'" % mo.group())
+        break
+
+
+
+
+
