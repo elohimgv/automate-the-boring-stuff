fda077df2a37057ae243cd1112d3691154a979d2
Elohim <elohimgv@users.noreply.github.com>
2020-08-28 14:55:50 -0500

    update: 05_multiple_groups_with_pip/multiple_groups_with_pip.py
    belongs to: 07_pattern_matching_with_regular_expressions

--- 07_pattern_matching_with_regular_expressions/05_multiple_groups_with_pip/multiple_groups_with_pip.py
+++ 07_pattern_matching_with_regular_expressions/05_multiple_groups_with_pip/multiple_groups_with_pip.py
@@ -3,11 +3,11 @@
 import re
 from random import choice 
 
-# Creating a regex object
-planetRegex = re.compile(r'Sat(urn|ori|o|iro)')
+# A|B, creates an RE that will match either A or B
+planetRegex = re.compile(r'Sat(urn|ori|o|iro)') # Creating a regex object
 txt = ('urn', 'ori', 'o', 'iro')
 slectText = choice(txt)
 # Matching object, string that will find
 mo = planetRegex.search('Sat' + slectText)
-# Call match object group() to return the matching text string
+# Call match object with group() to return the matching text string
 print(mo.group())
