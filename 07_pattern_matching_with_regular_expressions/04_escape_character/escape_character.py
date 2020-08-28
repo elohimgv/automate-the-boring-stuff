0b78a2d618db99981e1a4eceb82076c4654ea701
Elohim <elohimgv@users.noreply.github.com>
2020-08-28 14:53:20 -0500

    update:  04_escape_character/escape_character.py
    belongs to: 07_pattern_matching_with_regular_expressions

--- 07_pattern_matching_with_regular_expressions/04_escape_character/escape_character.py
+++ 07_pattern_matching_with_regular_expressions/04_escape_character/escape_character.py
@@ -2,9 +2,9 @@
 # Import module re
 import re
 
-# Creating a regex object
-phoneNumRegex = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d-\d\d)') # \(  \) to escape parentheses
+# \(  \) to escape parentheses
+phoneNumRegex = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d-\d\d)') # Creating a regex object
 # Matching object, string that will find
 mo = phoneNumRegex.search("My cell phone numeber is (678) 789-90-90")
-# Call match object group() to return the matching text string
+# Call match object with group() to return the matching text string
 print(mo.group())
