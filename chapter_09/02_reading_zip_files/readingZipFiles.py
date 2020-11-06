c5ff90e798b11f836702edde9ccf6dc2e3a2dd65
elohim <elohim.guevara@gmail.com>
2020-11-06 12:19:00 -0600

    add: 02_reading_zip_files/readingZipFiles.py
    belongs to: chapter_09

---
+++ chapter_09/02_reading_zip_files/readingZipFiles.py
@@ -0,0 +1,15 @@
+# readingZipFiles.py - Read the content of zip file
+
+import zipfile, os
+
+# Move to the folder with example.zip
+os.chdir('./Documets') 
+# Object zip file
+exampleZip = zipfile.ZipFile('example.zip')
+print(exampleZip.namelist()) 
+# Return a list for all the files and folders contained
+spamInfo = exampleZip.getinfo('spam.txt')
+print(spamInfo.file_size)
+print(spamInfo.compress_size)
+print('Compressed file is %sx smaller!' % (round(spamInfo.file_size / spamInfo.compress_size, 2)))
+exampleZip.close()
